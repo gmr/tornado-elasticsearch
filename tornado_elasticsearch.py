4e2478fb8e1a63ddfb0790a1f1083d7d4ee15db7
Nikita Solovyev <solovyev.nik@gmail.com>
2017-01-18 11:22:31 +0300

    - Added method to call `_cluster/health` to use instead of `info`   method, which is no longer supported in newer Elasticsearch server versions.
    Signed-off-by: Nikita Solovyev <solovyev.nik@gmail.com>

--- tornado_elasticsearch.py
+++ tornado_elasticsearch.py
@@ -262,6 +262,22 @@ class AsyncElasticsearch(Elasticsearch):
         raise gen.Return(data)
 
     @gen.coroutine
+    def health(self, params=None):
+        """Coroutine. Queries cluster Health API.
+
+        Returns a 2-tuple, where first element is request status, and second
+        element is a dictionary with response data.
+
+        :param params: dictionary of query parameters, will be handed over to
+            the underlying :class:`~torando_elasticsearch.AsyncHTTPConnection`
+            class for serialization
+
+        """
+        status, data = yield self.transport.perform_request(
+            "GET", "/_cluster/health", params=params)
+        raise gen.Return((status, data))
+
+    @gen.coroutine
     @query_params('consistency', 'id', 'parent', 'percolate', 'refresh',
                   'replication', 'routing', 'timeout', 'timestamp', 'ttl',
                   'version', 'version_type')
